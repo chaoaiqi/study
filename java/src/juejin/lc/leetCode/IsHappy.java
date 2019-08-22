5b73cfdf60aefe86ee88cb9787e02a1320124af5
liuchao <lc91151@163.com>
2019-08-22 17:29:08 +0800

    简单算法练习 2019年6月20日

---
+++ java/src/juejin/lc/leetCode/IsHappy.java
@@ -0,0 +1,34 @@
+package juejin.lc.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 肥宅快乐数
+ *
+ * @author liuchao
+ * @date 2019/7/15
+ */
+public class IsHappy {
+    private Map map = new HashMap();
+    /**
+     * 一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，
+     * 也可能是无限循环但始终变不到 1。如果可以变为 1，那么这个数就是快乐数。
+     * 输入: 19
+     * 输出: true
+     * 解释:
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+     *
+     * @param n 快乐数
+     * @return 返回结果
+     */
+    private boolean solution(int n) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ java/src/juejin/lc/leetCode/NarcissisticNumber.java
@@ -0,0 +1,27 @@
+package juejin.lc.leetCode;
+
+/**
+ * 水仙花数
+ * 求1000以内的水仙花数
+ * 153，370，371，407
+ * 153 = 1*1*1 + 5*5*5 + 3 * 3 * 3;
+ * @author liuchao
+ * @date 2019/8/22
+ */
+public class NarcissisticNumber {
+    private static  void solution (){
+        // 首先一定是三位数
+        for (int i = 100; i < 1000; i++) {
+            int unit = i % 10;
+            int ten = (i / 10) % 10;
+            int hundred = (i / 10 / 10) % 10;
+            if(i == (unit * unit * unit + ten * ten * ten + hundred * hundred * hundred)){
+                System.out.println(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
